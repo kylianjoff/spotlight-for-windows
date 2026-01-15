98fa93c4aefd679646d5bd1310efe4900ee6b1d2
kylianjoff <kylianju382@gmail.com>
2026-01-15 17:33:31 +0100

    autoUpdater preloads

--- preload.js
+++ preload.js
@@ -6,6 +6,21 @@ contextBridge.exposeInMainWorld('electronAPI', {
   openFile: (filePath) => ipcRenderer.invoke('open-file', filePath),
   getAppIcon: (appPath) => ipcRenderer.invoke('get-app-icon', appPath),
   openUrl: (url) => ipcRenderer.invoke('open-url', url),
+
+  // Auto-launch
   getAutoLaunchStatus: () => ipcRenderer.invoke('get-autolaunch-status'),
-  setAutoLaunch: (enabled) => ipcRenderer.invoke('set-autolaunch', enabled)
+  setAutoLaunch: (enabled) => ipcRenderer.invoke('set-autolaunch', enabled),
+
+  // Mises à jour
+  checkForUpdates: () => ipcRenderer.invoke('check-for-updates'),
+  getUpdateStatus: () => ipcRenderer.invoke('get-update-status'),
+  downloadUpdate: () => ipcRenderer.invoke('download-update'),
+  installUpdate: () => ipcRenderer.invoke('install-update'),
+
+  // Ecouter les événements de mise à jour
+  onUpdateAvailable: (callback) => ipcRenderer.on('update-available', (_, data) => callback(data)),
+  onUpdateNotAvailable: (callback) => ipcRenderer.on('update-not-available', () => callback()),
+  onDwnloadProgress: (callback) => ipcRenderer.on('download-progress', (_, data) => callback(data)),
+  onUpdateDownloaded: (callback) => ipcRenderer.on('update-downloaded', (_, data) => callback(data)),
+  onUpdateError: (callback) => ipcRenderer.on('update-error', (_, error) => callback(error))
 });
